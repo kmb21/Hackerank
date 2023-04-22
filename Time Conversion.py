477640daf3c6a7940fbd1ab1a73ab244c672bdb7
kmb21 <113995857+kmb21@users.noreply.github.com>
2023-04-22 11:30:25 -0400

    added improved solution

--- Time Conversion.py
+++ Time Conversion.py
@@ -1,5 +1,5 @@
 def timeConversion(s):
-    # Write your code here
+    #Version 1 beginner
     components = s.split(":")
     if "AM" in components[2]:
         components[2] = components[2].replace("AM", '')
@@ -15,3 +15,18 @@ def timeConversion(s):
             new = int(components[0]) + 12
             components[0] = str(new)
             return "%s:%s:%s"%(components[0], components[1], components[2]) 
+        
+        
+def timeConversion(s):
+   #Version 2 after I have grown
+    t = s.split(":")
+    if "PM" in t[2]:
+        if int(t[0]) == 12:
+            return "%s:%s:%s"%(t[0],t[1],t[2][:2])
+        else:
+            t[0] = str(int(t[0]) + 12)
+            return "%s:%s:%s"%(t[0],t[1],t[2][:2])
+    elif "AM" in t[2] and int(t[0]) == 12:
+        return "00:%s:%s"%(t[1], t[2][:2])
+    else:
+        return "%s:%s:%s"%(t[0],t[1],t[2][:2])
